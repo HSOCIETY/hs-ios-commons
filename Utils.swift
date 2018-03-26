3bc9e2c0682165c1aea8fe3a6ee6a105e52961a3
박요섭 <yoseop.park@hsociety.co.kr>
2018-03-26 17:49:16 +0900

    - 샘플 유틸 추가.

---
+++ Utils.swift
@@ -0,0 +1,15 @@
+//
+//  Utils.swift
+//  HSSwiftCommon
+//
+//  Created by yoseop park on 2018. 3. 26..
+//
+
+import UIKit
+
+class Utils: NSObject {
+
+    func myFirstPods() -> String {
+        return "Hello World~~~~"
+    }
+}
