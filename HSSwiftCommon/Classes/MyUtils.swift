6d99578efeb0179786d66e70209160f2ebeddf67
박요섭 <yoseop.park@hsociety.co.kr>
2018-03-27 10:41:33 +0900

    - 111

--- HSSwiftCommon/Classes/MyUtils.swift
+++ HSSwiftCommon/Classes/MyUtils.swift
@@ -9,26 +9,26 @@ import UIKit
 
 public class MyUtils: NSObject {
 
-   func myFirstPods() -> String {
+   public func myFirstPods() -> String {
     print("------------------myFirstPods")
         return "Hello World~~~~"
     }
     
-    func mySecondPods() -> String {
+    public func mySecondPods() -> String {
         print("------------------mySecondPods")
         return "Hay Man~"
     }
     
-//    public static func callPrivateString() -> String {
-//        print("------------------callPrivateString")
-//        return myprivateFunc()
-//    }
+    public static func callPrivateString() -> String {
+        print("------------------callPrivateString")
+        return myprivateFunc()
+    }
 }
 
-//private extension MyUtils {
-//    static func myprivateFunc() -> String {
-//        print("------------------myprivateFunc")
-//        return "Hay Man~"
-//    }
-//}
+private extension MyUtils {
+    static func myprivateFunc() -> String {
+        print("------------------myprivateFunc")
+        return "Hay Man~"
+    }
+}
 
---
+++ MyHsUtil.swift
@@ -0,0 +1,15 @@
+//
+//  MyHsUtil.swift
+//  HSSwiftCommon
+//
+//  Created by yoseop park on 2018. 3. 27..
+//
+
+import UIKit
+
+public class MyHsUtil: NSObject {
+
+    public func test111() {
+        print("test 111112")
+    }
+}
